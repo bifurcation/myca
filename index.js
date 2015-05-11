aab7bd92b885df1df3b85c3a5680a46575456679
Richard Barnes <richard.barnes@gmail.com>
2015-05-11 12:10:33 -0400

    Initially working server certificate generation

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 /*** Constants ***/
 
 var KEY_SIZE = 2048;
+var SERIAL_NUMBER_BYTES = 8;
 
 var FILE_ROOT_KEY = "root.key.pem";
 var FILE_ROOT_CERT = "root.cert.pem";
+var FILE_SERVER_KEY = "server.key.pem";
+var FILE_SERVER_CERT = "server.cert.pem";
 
 var CLASS_PROGRESS = "statusProgress";
 var CLASS_DONE = "statusDone";
@@ -18,6 +21,11 @@ var gRootKeyPair;
 var gRootCert;
 var gRootKeyPEM;
 var gRootCertPEM;
+
+var gServerKeyPair;
+var gServerCert;
+var gServerKeyPEM;
+var gServerCertPEM;
 
 
 /*** Utilities ***/
@@ -61,7 +69,7 @@ function fileLoader(callback) {
 function makeRoot() {
   var tag = "makeRoot";
 
-  /* Build the name from the entered name */
+  // Build the name from the entered name
   var enteredName = $("#rootName").val();
   if (!enteredName) {
     updateStatus(tag, "(enter a name)", CLASS_ERROR);
@@ -71,12 +79,12 @@ function makeRoot() {
     name: "commonName",
     value: enteredName 
   }];
-
-  /* Generate a key pair */
+  
+  // Generate a key pair
   updateStatus(tag, "(generating key)", CLASS_PROGRESS);
   gRootKeyPair = forge.pki.rsa.generateKeyPair(KEY_SIZE);
 
-  /* Create a self-signed certificate */
+  // Create a self-signed certificate
   gRootCert = forge.pki.createCertificate();
   gRootCert.publicKey = gRootKeyPair.publicKey;
   gRootCert.serialNumber = "01";
@@ -103,11 +111,11 @@ function makeRoot() {
     gRootCert.sign(gRootKeyPair.privateKey);
   } catch (err) {
     updateStatus(tag, "(error signing certificate)", CLASS_ERROR);
-    console.log(err);
+    console.log("Error signing certificate" + err);
     return;
   }
 
-  /* Prepare the key and certificate for download */
+  // Prepare the key and certificate for download
   gRootKeyPEM = forge.pki.privateKeyToPem(gRootKeyPair.privateKey);
   gRootCertPEM = forge.pki.certificateToPem(gRootCert);
   updateStatus(tag, "(done)", CLASS_DONE);
@@ -155,26 +163,101 @@ function checkServerNames() {
   const hostname = /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/;
   
   // Should be whitespace-separated DNS names
-  var serverNames = $(this).val().split(/\s+/);
+  var serverNames = $("#serverNames").val().split(/\s+/);
   
   if (serverNames.length == 0) {
     console.log("Need a name");
     updateStatus(tag, "(need a name)", CLASS_PENDING);
-    return
+    return false;
   }
 
   for (var i=0; i < serverNames.length; ++i) {
     if (!serverNames[i].match(hostname)) {
       console.log("Invalid name: " + serverNames[i]);
       updateStatus(tag, "(invalid)", CLASS_ERROR);
-      return;
+      return false;
     }
   }
 
-  console.log("ok");
   updateStatus(tag, "(ok)", CLASS_DONE);
+  return serverNames;
 }
 
+function makeServer() {
+  var tag = "makeServer";
+
+  // Check that the root has been loaded 
+  if (!gRootKeyPair || !gRootKeyPair.privateKey || !gRootCert) {
+    updateStatus(tag, "(no root)", CLASS_ERROR);
+    return;
+  }
+
+  // Check the server names
+  var serverNames = checkServerNames();
+  if (!serverNames) {
+    updateStatus(tag, "(bad names)", CLASS_ERROR);
+    return;
+  }
+  var subjectName = [{
+    name: "commonName",
+    value: serverNames[0]
+  }];
+  var altNames = serverNames.map(function(name) {
+    return { type: 2, value: name };
+  });
+
+  // Generate a key pair
+  updateStatus(tag, "(generating key)", CLASS_PROGRESS);
+  gServerKeyPair = forge.pki.rsa.generateKeyPair(KEY_SIZE);
+
+  // Generate a random serial number
+  var serialNumber = forge.random.getBytesSync(SERIAL_NUMBER_BYTES);
+  var serialNumberHex = "00" + forge.util.bytesToHex(serialNumber);
+
+  // Create a certificate signed by the CA
+  gServerCert = forge.pki.createCertificate();
+  gServerCert.publicKey = gServerKeyPair.publicKey;
+  gServerCert.serialNumber = serialNumberHex;
+  gServerCert.validity.notBefore = new Date();
+  gServerCert.validity.notAfter = new Date(gRootCert.validity.notAfter);
+  gServerCert.setSubject(subjectName);
+  gServerCert.setIssuer(gRootCert.issuer.attributes);
+  gServerCert.setExtensions([{
+    name: 'basicConstraints',
+    cA: false
+  }, {
+    name: 'keyUsage',
+    digitalSignature: true,
+    keyEncipherment: true,
+    dataEncipherment: true
+  }, {
+    name: 'extKeyUsage',
+    serverAuth: true,
+  }, {
+    name: 'subjectAltName',
+    altNames: altNames
+  }, {
+    name: 'subjectKeyIdentifier'
+  }]);
+    
+  try {
+    updateStatus(tag, "(signing certificate)", CLASS_PROGRESS);
+    gServerCert.sign(gRootKeyPair.privateKey);
+  } catch (err) {
+    updateStatus(tag, "(error signing certificate)", CLASS_ERROR);
+    console.log("Error signing certificate" + err);
+    return;
+  }
+  
+  // Prepare the key and certificate for download
+  gServerKeyPEM = forge.pki.privateKeyToPem(gServerKeyPair.privateKey);
+  gServerCertPEM = forge.pki.certificateToPem(gServerCert);
+  updateStatus(tag, "(done)", CLASS_DONE);
+  return;
+}
+
+function downloadServerKey()  { saveFile(FILE_SERVER_KEY, gServerKeyPEM); }
+function downloadServerCert() { saveFile(FILE_SERVER_CERT, gServerCertPEM); }
 
 /*** Ready handler ***/
 
@@ -186,4 +269,7 @@ $(document).ready(function() {
   $("#rootKey").change(fileLoader(loadRootKey));
   $("#rootCert").change(fileLoader(loadRootCert));
   $("#serverNames").keyup(checkServerNames);
+  $("#makeServer").click(makeServer);
+  $("#downloadServerKey").click(downloadServerKey);
+  $("#downloadServerCert").click(downloadServerCert);
 });
